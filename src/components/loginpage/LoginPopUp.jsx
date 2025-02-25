c9995d3dd57c8dff33f05b8c2e5ed69d8e61ad25
Nihal <nihal.pm9633@gmail.com>
2025-02-25 11:23:31 +0530

    login signup page design changed

--- src/components/loginpage/LoginPopUp.jsx
+++ src/components/loginpage/LoginPopUp.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import './LoginPopUp.css'
 import { server } from '../../server';
 import { toast } from 'react-toastify';
+import bg from '../../img/bg.svg'
 
 const LoginPopUp = () => {
 
@@ -27,49 +28,51 @@ const LoginPopUp = () => {
 
   return (
 
-    <>
-        <div className="wrappers">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
-            </form>
-            <button type='submit'>Login</button>
+    <div className="container">
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form onSubmit={handleSubmit} className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </div>
+            <div className="input-field">
+              <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+          </form>
         </div>
-    </>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>WELCOME BACK</h3>
+            <p>
+              You don't have account please register, Enjoy services
+            </p>
+            <button className="btn transparent" id="sign-up-btn">
+              Sign up
+            </button>
+          </div>
+          <img src={bg} className="image" alt="" />
+        </div>
+      </div>
+    </div>
 
 
+    // <>
+    //     <div className="wrappers">
+    //         <h1>Login</h1>
+    //         <form onSubmit={handleSubmit}>
+    //         <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+    //         <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
+    //         </form>
+    //         <button type='submit'>Login</button>
+    //     </div>
+    // </>
 
 
-    // <div className='login-popup'> 
-    //     <form className="login-popup-container" onSubmit={handleSubmit}>
-    //         <div className="login-popup-title">
-    //             <h2>{currState}</h2>
-    //             <img onClick={()=> setShowLogin(false)} alt="" />
-    //         </div>
-    //         <div className="login-popup-inputs">
-
-    //         {currState === "Login" && (
-    //             <>
-    //             <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
-    //             <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
-    //             </>
-    //             )}
-
-    //             {/* Username & Password (Common for both Login & Signup) */}
-                
-    //         </div>
-    //         <button type='submit'>{currState === "Sign Up" ? "Create account" : "Login"}</button>
-    //         <div className="login-popup-condition">
-    //             <input type="checkbox" required/>
-    //             <p>By continuing, i agree to the terms of use & privacy policy.</p>
-    //         </div>
-    //         {/* {currState === "Login"
-    //         ?<p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
-    //         :<p>Already have an account? <span onClick={()=> setCurrState("Login")}>Login here</span></p>
-    //         } */}
-    //     </form>
-    // </div>
   )
 }
 
--- src/components/signuppage/SignUpPopUp.jsx
+++ src/components/signuppage/SignUpPopUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import './SignUpPopUp.css'
+import bg from '../../img/bg.svg'
 import { server } from '../../server'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
@@ -40,57 +41,76 @@ const SignUpPopUp = () => {
 
   return (
 
-    <>
-        <div className="wrapper">
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Shop name" required value={shopname} onChange={(e) => setShopname(e.target.value)}/>
-                 <input type="number" placeholder="Phone Number" required value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)}/>
-                 <input type="number" placeholder="WhatsApp Number" required value={whatsappno} onChange={(e) => setWhatsappno(e.target.value)}/>
-                 <input type="text" placeholder='TRN Number'required value={TRNno} onChange={(e) => setTRNno(e.target.value)}/>
-                 <input type="text" placeholder='Address' required value={address} onChange={(e) => setAddress(e.target.value)}/>
-                 <input type="text" placeholder="Location" required value={location} onChange={(e) => setLocation(e.target.value)}/>
-                 <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
-                 <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
-            </form>
-            <button type='submit'>Sign Up</button>
+    <div className="container">
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form onSubmit={handleSubmit} className="sign-in-form">
+            <h2 className="title">Sign Up</h2>
+            <div className="input-field">
+              <input type="text" placeholder="Shop Name" required value={shopname} onChange={(e) => setShopname(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="number" placeholder="Phone Number" required value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="number" placeholder="Whats app Number" required value={whatsappno} onChange={(e) => setWhatsappno(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="text" placeholder="TRN no" required value={TRNno} onChange={(e) => setTRNno(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="text" placeholder="Address" required value={address} onChange={(e) => setAddress(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="text" placeholder="Location" required value={location} onChange={(e) => setLocation(e.target.value)}/>
+            </div>
+            <div className="input-field">
+                <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </div>
+            <div className="input-field">
+              <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <input type="submit" value="Register" className="btn solid" />
+          </form>
+
         </div>
-    </>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here ?</h3>
+            <p>
+              You have already account please login, Enjoy the services
+            </p>
+            <button className="btn transparent" id="sign-up-btn">
+              Sign in
+            </button>
+          </div>
+          <img src={bg} className="image" alt="" />
+        </div>
+        
+      </div>
+    </div>
+
+    // <>
+    //     <div className="wrapper">
+    //         <h1>Sign Up</h1>
+    //         <form onSubmit={handleSubmit}>
+    //         <input type="text" placeholder="Shop name" required value={shopname} onChange={(e) => setShopname(e.target.value)}/>
+    //              <input type="number" placeholder="Phone Number" required value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)}/>
+    //              <input type="number" placeholder="WhatsApp Number" required value={whatsappno} onChange={(e) => setWhatsappno(e.target.value)}/>
+    //              <input type="text" placeholder='TRN Number'required value={TRNno} onChange={(e) => setTRNno(e.target.value)}/>
+    //              <input type="text" placeholder='Address' required value={address} onChange={(e) => setAddress(e.target.value)}/>
+    //              <input type="text" placeholder="Location" required value={location} onChange={(e) => setLocation(e.target.value)}/>
+    //              <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+    //              <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
+    //         </form>
+    //         <button type='submit'>Sign Up</button>
+    //     </div>
+    // </>
     
-    // <div className='login-popup'> 
-    //     <form className="login-popup-container" onSubmit={handleSubmit}>
-    //         <div className="login-popup-title">
-    //             <h2>{currState}</h2>
-    //             <img onClick={()=> setShowLogin(false)} alt="" />
-    //         </div>
-    //         <div className="login-popup-inputs">
-
-    //         {currState === "Sign Up" && (
-    //             <>
-    //             <input type="text" placeholder="Shop name" required value={shopname} onChange={(e) => setShopname(e.target.value)}/>
-    //             <input type="number" placeholder="Phone Number" required value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)}/>
-    //             <input type="number" placeholder="WhatsApp Number" required value={whatsappno} onChange={(e) => setWhatsappno(e.target.value)}/>
-    //             <input type="number" placeholder='TRN Number'required value={TRNno} onChange={(e) => setTRNno(e.target.value)}/>
-    //             <input type="text" placeholder='Address' required value={address} onChange={(e) => setAddress(e.target.value)}/>
-    //             <input type="text" placeholder="Location" required value={location} onChange={(e) => setLocation(e.target.value)}/>
-    //             </>
-    //             )}
-
-    //             {/* Username & Password (Common for both Login & Signup) */}
-    //             <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
-    //             <input type="password" placeholder="Your password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
-    //         </div>
-    //         <button type='submit'>{currState === "Sign Up" ? "Create account" : "Login"}</button>
-    //         <div className="login-popup-condition">
-    //             <input type="checkbox" required/>
-    //             <p>By continuing, i agree to the terms of use & privacy policy.</p>
-    //         </div>
-    //         {/* {currState === "Login"
-    //         ?<p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
-    //         :<p>Already have an account? <span onClick={()=> setCurrState("Login")}>Login here</span></p>
-    //         } */}
-    //     </form>
-    // </div>
+    
   )
 }
 
